14592594f8c5bb26da7ffc534b35ac2c488cab90
Miika Pihkola <miika.pihkola@gmail.com>
2024-04-15 18:20:44 +0300

    Selkeyttä "index.js"

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,9 @@ let index_array = [
     {
         'date': '15.4.2024',
         'text': '<h5  style="margin-bottom: 20px;">Miika Pihkola | Eurovaaliehdokas</h5>' +
+            // Lyhyesti minusta
             '<h6  style="margin-bottom: 20px;">Lyhyesti minusta</h6>Olen Lappeenrannassa asusteleva it-tradenomi opiskelija ja ikää on ehtinyt kertyä ' + years + ' vuotta. Opiskelut on vähän venähtäny kun olen etsinyt sitä sopivalta tuntuvaa alaa, mutta nyt koen löytäneeni sen itselleni sopivan. Opintojen ohella olen ollut aktiivinen muutamassa harrasteyhdistyksessä. Pisimpään olen ollut Pikametallimiehet ry:n hallituksessa, jossa olen ollut mukana tekemässä muutosta kerhosta yhdistykseksi vuonna 2018 ja tällähetkellä olen kyseisen yhdistyksen www-vastaava. Olen myös vuonna 2023 piipahtanut Pelikerho Louhi ry:n hallituksessa ja päädyin nykyisten opintojeni alussa opiskelu ryhmäni ryhmänedustajaksi, sekä onnistuin pääsemään 2024 keväällä mensan jäseneksi. Vapaa-ajan, siis sellaisen jota en käytä opiskeluun tai milloin mihinkin hupi projektiin, käytän usein pelaamiseen, kavereitten kanssa lautapelejä taikka sitten koneella jotain mielenkiintoista peliä.<br><br>' +
+            // Minä ja Liberaalipuolue
             '<h6  style="margin-bottom: 20px;">Minä ja Liberaalipuolue</h6>Liberaalipuolueen kaakon piiriin liityin vuonna 2023 eduskunta vaalien aikoihin, sillä olen aina kokenut ettei mikään isoista puolueista ole itselleni sopiva, "Miksi pitäisi valita vastuullisen rahankäytön ja liberaalin vapausaatten väliltä?", mutta noissa vaaleissa törmäsin omaa ehdokasta etsiessäni tähän sopivaan pienpuolueeseen. Liittymisen jälkeen jo heti vuonna 2024 päädyin kaakon piirin hallitukseen ja eurovaaliehdokkaaksi.'
     }
 ]
